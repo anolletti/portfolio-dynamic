185e1c42937bb0c61686203ca39adb93e15d83a1
anolletti <anthonynolletti@gmail.com>
2022-12-03 17:39:09 -0500

    Initial upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from forms import CourseForm
+import smtplib, os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+MY_EMAIL = "anthonynolletti@gmail.com"
+MY_PASSWORD = os.environ['MY_PASSWORD']
+app.config['RECAPTCHA_PUBLIC_KEY']= os.environ['RECAPTCHA_PUBLIC_KEY']
+app.config['RECAPTCHA_PRIVATE_KEY']= os.environ['RECAPTCHA_PRIVATE_KEY']
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = CourseForm()
+    if form.validate_on_submit():
+        sender_name = form.name.data
+        sender_email = form.email.data
+        message = form.message.data
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=[MY_EMAIL, sender_email],
+                msg=f"Subject:{sender_name}'s Inquiry\n\nHello {sender_name}, \n\nBelow is the inquiry you submitted on my site:\n\n*****************************\n\n{message}\n\n*****************************\n\nI will get back to you shortly! \n\nSincerely,\n\nAnthony"
+        )
+
+        return render_template("success.html", sender_name=sender_name, sender_email=sender_email, message=message)
+   
+    return render_template('index.html', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import (StringField, TextAreaField)
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired
+
+class CourseForm(FlaskForm):
+    recaptcha = RecaptchaField()
+
+    name = StringField('Name', validators=[InputRequired()
+                                             ])
+    message = TextAreaField('Message',
+                                validators=[InputRequired(),
+                                           ])
+    email = EmailField('Email Address',
+                                validators=[InputRequired()])
+                                            
+    
